1fe556f859f556a103ef5a75c9439e2f1e933e07
sararabaglia <61978272+sararabaglia@users.noreply.github.com>
2020-12-28 16:14:12 +0100

    Update myCutFlow.py

--- myCutFlow.py
+++ myCutFlow.py
@@ -48,3 +48,62 @@ for j_entry in range(entries1):
 print(count1)
 print(count2)
 print(count3)
+
+count1b = 0
+count2b = 0
+count3b = 0
+
+for i_entry in range(entries2):
+ mytree2.GetEvent(i_entry)
+ if (mytree2.GetLeaf("met").GetValue()>150.0 and mytree2.GetLeaf("nJet30").GetValue()>=2 and mytree2.GetLeaf("nJet30").GetValue()<4 and mytree2.GetLeaf("nLep_base").GetValue()==1 and mytree2.GetLeaf("nLep_signal").GetValue()==1 and mytree2.GetLeaf("mt").GetValue()>50):
+  count1b = count1b +1
+  if mytree2.GetLeaf("nBJet30_MV2c10").GetValue()==2:
+      if mytree2.GetLeaf("mbb").GetValue()>50:
+          if mytree2.GetLeaf("mbb").GetValue()>105 and mytree2.GetLeaf("mbb").GetValue()<135 :
+              if mytree2.GetLeaf("mct2").GetValue()>160 :
+                  if mytree2.GetLeaf("met").GetValue()>200 :
+                      count2b = count2b + 1
+                      if mytree2.GetLeaf("mt").GetValue()>100 and mytree2.GetLeaf("mt").GetValue()<140 and mytree2.GetLeaf("met").GetValue()>200 and mytree2.GetLeaf("nJet30").GetValue()>=2 and mytree2.GetLeaf("nJet30").GetValue()<4 and mytree2.GetLeaf("mct2").GetValue()>160 and mytree2.GetLeaf("nLep_base").GetValue()==1 and mytree2.GetLeaf("nLep_signal").GetValue()==1 and mytree2.GetLeaf("mt").GetValue()>50 and mytree2.GetLeaf("nBJet30_MV2c10").GetValue()==2 and mytree2.GetLeaf("mbb").GetValue()>105 and mytree2.GetLeaf("mbb").GetValue()<135:
+                       count3b = count3b + 1
+                      else: continue
+                  else: continue
+              else: continue
+          else: continue
+      else: continue
+  else: continue
+ else: continue
+  
+print(count1b)
+print(count2b)
+print(count3b)
+
+count1c = 0
+count2c = 0
+count3c = 0
+
+for w_entry in range(entries1):
+ mytree3.GetEvent(w_entry)
+ if (mytree3.GetLeaf("met").GetValue()>150.0 and mytree3.GetLeaf("nJet30").GetValue()>=2 and mytree3.GetLeaf("nJet30").GetValue()<4 and mytree3.GetLeaf("nLep_base").GetValue()==1 and mytree3.GetLeaf("nLep_signal").GetValue()==1 and mytree3.GetLeaf("mt").GetValue()>50):
+  count1c = count1c +1
+  if mytree3.GetLeaf("nBJet30_MV2c10").GetValue()==2:
+      if mytree3.GetLeaf("mbb").GetValue()>50:
+          if mytree3.GetLeaf("mbb").GetValue()>105 and mytree3.GetLeaf("mbb").GetValue()<135 :
+              if mytree3.GetLeaf("mct2").GetValue()>160 :
+                  if mytree3.GetLeaf("met").GetValue()>200 :
+                      count2c = count2c + 1
+                      if mytree3.GetLeaf("mt").GetValue()>100 and mytree3.GetLeaf("mt").GetValue()<140 and mytree3.GetLeaf("met").GetValue()>200 and mytree3.GetLeaf("nJet30").GetValue()>=2 and mytree3.GetLeaf("nJet30").GetValue()<4 and mytree3.GetLeaf("mct2").GetValue()>160 and mytree3.GetLeaf("nLep_base").GetValue()==1 and mytree3.GetLeaf("nLep_signal").GetValue()==1 and mytree3.GetLeaf("mt").GetValue()>50 and mytree3.GetLeaf("nBJet30_MV2c10").GetValue()==2 and mytree3.GetLeaf("mbb").GetValue()>105 and mytree3.GetLeaf("mbb").GetValue()<135:
+                       count3c = count3c + 1
+                      else: continue
+                  else: continue
+              else: continue
+          else: continue
+      else: continue
+  else: continue
+ else: continue
+
+print(count1c)
+print(count2c)
+print(count3c)
+
+
+
