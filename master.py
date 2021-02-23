ed378ee4e74b7b1e97356470da6545d24771672d
sararabaglia <61978272+sararabaglia@users.noreply.github.com>
2021-02-23 17:32:42 +0100

    Update master.py

--- master.py
+++ master.py
@@ -2,4 +2,12 @@ import os
 
 os.system ("python myCutFlow.py")
 
-print("do you ")
+print("do you want analyze the data also with TMVA using the BDT?[y/n]")
+
+tmva = input()
+
+if (tmva == 'y'):
+ os.system ("root")
+ os.system (".L tmva_cut.C")
+ os.system ("tmva_cut()")
+
